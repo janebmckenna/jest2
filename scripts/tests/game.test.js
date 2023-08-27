34ec884834dc8925959fce6e7a9a7c77e24c81c5
jane mckenna <janemckenna@hotmail.com>
2023-08-27 10:47:06 +0000

    inital notes

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,29 @@
+//@jest-environment jsdom
+
+const game = require('../game');
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe('game object contains correct keys', () => {
+    test('score key exists', ()=> {
+        expect('score' in game).toBe(true);
+    });
+    test('current game key exists', ()=> {
+        expect('currentGame' in game).toBe(true);
+    });
+    test('playerMoves key exists', ()=> {
+        expect('currentGame' in game).toBe(true);
+    });
+    test('choices key exists', ()=> {
+        expect('currentGame' in game).toBe(true);
+    });
+    test('choices contains the correct IDs', () => {
+        expect(game.choices).toEqual(['button1', 'button2', 'button3', 'button4']);
+    });
+});
